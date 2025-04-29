407f0d37da35988f63de2dec90ce916465c2bc64
Yuri Kauan <yurrikauan@gmail.com>
2025-04-29 08:47:36 -0300

    change

--- exercicio5.py
+++ exercicio5.py
@@ -1,2 +1,11 @@
-for i in range(10, 0, -1):
-    print(i)
+array = []
+
+print("Quando você quiser finalizar, digite -> -1")
+while True:
+    x = int(input("Digite um número: "))
+    array.append(x)
+
+    if x == -1:
+        break
+
+print(f"Seu array final é: {array}")
--- exercicio6.py
+++ exercicio6.py
@@ -1,3 +1,3 @@
-x = 100
-for i in range(x + 1,x + 11, 1):
+arrayCompras = ["banana", "laranja", "maçã"]
+for i in arrayCompras:
     print(i)
