959a4418dea3d9b41092ab82eac22e90b294e3d1
Yuri Kauan <yurrikauan@gmail.com>
2025-04-29 09:07:49 -0300

    using array methods

--- exercicio6.py
+++ exercicio6.py
@@ -1,3 +1,15 @@
-arrayCompras = ["banana", "laranja", "maçã"]
-for i in arrayCompras:
-    print(i)
+nomes = [""]*5
+
+i = 0
+
+while True:
+    nomepedido = str(input("Digite um nome: "))
+    nomes.insert(i, nomepedido)
+    i += 1
+
+    for i2 in nomes:
+        if i2 == "":
+            nomes.pop()
+    if i == 5:
+        print(nomes)
+        break
