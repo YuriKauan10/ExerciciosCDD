adefc75b599b085dede6ab5077a4bb5d64423d65
YuriKauan10 <yurrikauan@gmail.com>
2025-05-15 14:27:12 -0300

    more exercise for added

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+
+print("Números Pares: ")
+
+for i in range(1, 20):
+    if i % 2 == 0:
+        print(i)
+print( )
+print("Números pares ao contrário: ")
+
+for i in range(20, 1, -1):
+    if i % 2 == 0:
+        print(i)
